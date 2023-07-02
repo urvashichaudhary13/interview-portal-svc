7477e356a680c2abf1568d0aa75bbc283d918ed5
UrvashiChaudhary13 <urvashichaudhary2513@gmail.com>
2023-07-02 15:08:32 +0530

    add validations

--- src/validations.ts
+++ src/validations.ts
@@ -112,5 +112,76 @@ export const validations = {
       required: true,
       isString: true,
     },
+  },
+  createCandidate: {
+    name: {
+      options: (name: string) => {
+        if (!name) {
+          throw 'Please provide name';
+        } else if (name.length > 200) {
+          throw 'name can not be more than 200 characters.';
+        }
+        return true;
+      },
+      errorMessage: 'Name is wrong be must a String',
+      exists: {
+        errorMessage: 'Please Provide name',
+      },
+      in: ['body'],
+      required: true,
+      isString: true,
+    },
+    email: {
+      isEmail: {
+        bail: true,
+      },
+      unique: true,
+      custom: {
+        options: (email: string) => {
+          if (!email) {
+            throw 'Please provide email';
+          } else if (!validateEmail(email)) {
+            throw 'Please provide valid email';
+          }
+          return true;
+        }
+      },
+      in: ['body'],
+      required: true,
+      errorMessage: 'Please provide a valid email id',
+    },
+    experience: {
+      options: (experience: number) => {
+        if (!experience) {
+          throw 'Please provide experience in years';
+        }
+        return true;
+      },
+      errorMessage: 'Experience is wrong, must be a number',
+      exists: {
+        errorMessage: 'Please Provide experience',
+      },
+      in: ['body'],
+      required: true,
+      isString: true,
+    },
+    jobProfile: {
+      options: (jobProfile: string) => {
+        if (!jobProfile) {
+          throw 'Please provide jobProfile';
+        }
+        return true;
+      },
+      errorMessage: 'jobProfile is wrong, must be a String',
+      exists: {
+        errorMessage: 'Please Provide jobProfile',
+      },
+      in: ['body'],
+      required: true,
+      isString: true,
+    },
+  },
+  getCandidates: {
+    
   }
 }
